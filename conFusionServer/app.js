39fb877022093d706d3f84f2e2a8890682f4bb29
akash koshta <akoshta18@gmail.com>
2018-05-25 12:55:50 +0530

    Express Rest api 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -11,6 +11,16 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose =require('mongoose');
+mongoose.Promise = require('bluebird');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+  console.log('Connected correctly to server');
+  var db = mongoose.connection;
+},(err)=>{ console.log(err); });
+
 var app = express();
 
 // view engine setup
