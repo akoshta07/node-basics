f85db2c55becfe1419629896a560f07db4338e09
akash koshta <akoshta18@gmail.com>
2018-06-11 12:54:52 +0530

    Cors

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -3,20 +3,25 @@ const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+const cors = require('./cors');
 
 var router = express.Router();
 /* GET users listing. */
-router.get('/' ,authenticate.verifyUser ,authenticate.verifyAdmin ,function(req, res, next) {
+router.get('/',cors.corsWithOptions ,authenticate.verifyUser ,authenticate.verifyAdmin ,function(req, res, next) {
   User.find({})
    .then((users) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json(users);
-}, (err) => next(err))
-.catch((err) => next(err));
+})
+.catch((err) => {
+  res.statusCode = 500;
+  res.setHeader('Content-Type','application/json');
+  res.json({err: err});
+  });
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup',cors.corsWithOptions , (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
@@ -46,7 +51,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login',cors.corsWithOptions , passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
